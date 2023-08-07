0e7aac41b3d7ffdf98320c126227b907b9424a64
EntityJoKeR <133527371+EntityJoKeR@users.noreply.github.com>
2023-08-08 03:46:07 +0600

    по мелочи

--- handlers123/shop/market.py
+++ handlers123/shop/market.py
@@ -7,37 +7,54 @@ database = MongoClient(
 marketplace_collection = database['marketplace']
 
 
-
-marketplace_collection.insert_one({'id':1234})
-
-
-class Marketplase:
-    def __init__():
-        ...
-        
-    async def sale_oil(callback: types.CallbackQuery) -> None:
+class Marketplace:
+    def __init__(self):
         ...
 
-    async def sale_food(callback: types.CallbackQuery) -> None:
+    async def sale_oil(self, callback: types.CallbackQuery, price, volume) -> str:
+        marketplace_collection.insert_one({
+            'id': callback.from_user.id,
+            'product': 'oil',
+            'price': price,
+            'volume': volume
+        })
+        return 'Обьявление успешно добавлено'
+
+    async def sale_food(self, callback: types.CallbackQuery, price, volume) -> str:
+        marketplace_collection.insert_one({
+            'id': callback.from_user.id,
+            'product': 'food',
+            'price': price,
+            'volume': volume
+        })
+        return 'Обьявление успешно добавлено'
+
+
+    async def sale_car(self, callback: types.CallbackQuery, price, volume, name_car) -> str:
+        marketplace_collection.insert_one({
+            'id': callback.from_user.id,
+            'product': 'car',
+            'name_car': name_car,
+            'price': price,
+            'volume': volume
+        })
+        return 'Обьявление успешно добавлено'
+
+
+    async def buy_oil(self, callback: types.CallbackQuery) -> None:
         ...
 
-    async def sale_car(callback: types.CallbackQuery) -> None:
-        ...
+        async def buy_food(self, callback: types.CallbackQuery) -> None:
+            ...
 
-    async def buy_oil(callback: types.CallbackQuery) -> None:
-        ...
+        async def buy_car(self, callback: types.CallbackQuery) -> None:
+            ...
 
-    async def buy_food(callback: types.CallbackQuery) -> None:
-        ...
+        async def oil_list(self) -> list:
+            return marketplace_collection.find({'product': 'oil'})
 
-    async def buy_car(callback: types.CallbackQuery) -> None:
-        ...
+        async def food_list(self) -> list:
+            return marketplace_collection.find({'product': 'food'})
 
-    async def oil_list() -> list:
-        ...
-
-    async def food_list() -> list:
-        ...
-
-    async def cars_list() -> list:
-        ...
+        async def cars_list(self) -> list:
+            return marketplace_collection.find({'product': 'car'})
